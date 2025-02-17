ff9b1796edfe914f6afd9c94de4fd81949049f2b
ThePandaDever <calumbelcham@gmail.com>
2025-02-17 18:45:57 +0000

    smaller, and some other shit

--- src/rwl.js
+++ src/rwl.js
@@ -2,25 +2,28 @@
 const blockCache = {};
 const segmentCache = {};
 
-function splitBlock(text) {
+function split(text, type, name) {
     let text2 = text.split("\n").filter(t => t !== "").join("\n");
     text = text.trim();
     const tokens = [];
     let current = "";
 
-    let depth = 0;
+    let curlyDepth = 0,
+        squareDepth = 0;
     let hasSplit = false;
     let inSingle = false,
         inDouble = false,
         inTick = false;
     
+    const brackets = {"curly":["{","}"],"square":["[","]"]}[type] ?? ["",""]; // get the bracket pairs
+    const open = brackets[0],
+        close = brackets[1];
+    const splitChar = type.length === 1 ? type : "";
+    
     for (let i = 0; i < text.length; i++) {
         const char = text[i];
 
-        if (char == "\\") {
-            current += char;
-            continue;
-        }
+        if (char == "\\") { current += char; continue; }
 
         if (char == "'" && !(inDouble || inTick))
             inSingle = !inSingle;
@@ -37,209 +40,62 @@ function splitBlock(text) {
         }
 
         if (char === "{")
-            depth ++;
+            curlyDepth ++;
         if (char === "}")
-            depth --;
-
-        if (char === "{" && depth == 1 && !hasSplit) {
-            tokens.push(current.trim());
-            current = "";
-            continue;
-        }
-        if (char === "}" && depth == 0 && !hasSplit) {
-            hasSplit = true;
-            continue;
-        }
-
-        if (hasSplit) {
-            throw Error("Unexpected text after block: \n" + text.substring(i).trim().split("\n").map(t => "    " + t).join("\n") + "\nin:\n" + text2.split("\n").map(t => "    " + t).join("\n") + "\n");
-        }
-
-        current += char;
-    }
-
-    if (current) {
-        tokens.push(current.trim());
-    }
-
-    return tokens;
-}
-
-function splitHeader(text) {
-    let text2 = text.split("\n").filter(t => t !== "").join("\n");
-    text = text.trim();
-    const tokens = [];
-    let current = "";
-
-    let depth = 0;
-    let hasSplit = false;
-    let inSingle = false,
-        inDouble = false,
-        inTick = false;
-    
-    for (let i = 0; i < text.length; i++) {
-        const char = text[i];
-
-        if (char == "\\") {
-            current += char;
-            continue;
-        }
-
-        if (char == "'" && !(inDouble || inTick))
-            inSingle = !inSingle;
-        if (char == "\"" && !(inSingle || inTick))
-            inDouble = !inDouble;
-        if (char == "`" && !(inSingle || inDouble))
-            inTick = !inTick;
-
-        const inQuotes = inSingle || inDouble || inTick;
-
-        if (inQuotes) {
-            current += char;
-            continue;
-        }
-
-        if (char === "[")
-            depth ++;
-        if (char === "]")
-            depth --;
-
-        if (char === "[" && depth == 1) {
-            tokens.push(current.trim());
-            current = "";
-            continue;
-        }
-        if (char === "]" && depth == 0 && !hasSplit) {
-            hasSplit = true;
-            if (current)
-                tokens.push(current.trim());
-            current = "";
-            continue;
-        }
-
-        if (hasSplit) {
-            throw Error("Unexpected text after header attributes: \n" + text.substring(i).trim().split("\n").map(t => "    " + t).join("\n") + "\nin:\n" + text2.split("\n").map(t => "    " + t).join("\n") + "\n");
-        }
-
-        current += char;
-    }
-
-    if (current) {
-        tokens.push(current.trim());
-    }
-
-    return tokens;
-}
-
-function splitSegment(text) {
-    text = text.trim();
-    const tokens = [];
-    let current = "";
-
-    let depth = 0;
-    let squareDepth = 0;
-    let inSingle = false;
-        inDouble = false;
-        inTick = false;
-    
-    for (let i = 0; i < text.length; i++) {
-        const char = text[i];
-
-        if (char == "\\") {
-            current += char;
-            continue;
-        }
-
-        if (char == "'" && !(inDouble || inTick))
-            inSingle = !inSingle;
-        if (char == "\"" && !(inSingle || inTick))
-            inDouble = !inDouble;
-        if (char == "`" && !(inSingle || inDouble))
-            inTick = !inTick;
-
-        const inQuotes = inSingle || inDouble || inTick;
-
-        if (inQuotes) {
-            current += char;
-            continue;
-        }
-
-        if (char === "{")
-            depth ++;
-        if (char === "}")
-            depth --;
-
+            curlyDepth --;
         if (char === "[")
             squareDepth ++;
         if (char === "]")
             squareDepth --;
+        
+        if (char === open && curlyDepth == (type == "curly" ? 1 : 0) && squareDepth == (type == "square" ? 1 : 0) && !hasSplit) {
+            tokens.push(current.trim());
+            current = "";
+            continue;
+        }
+        if (char === close && curlyDepth == 0 && squareDepth == 0 && !hasSplit) {
+            hasSplit = true;
+            continue;
+        }
 
-        if (char === "," && depth == 0 && squareDepth == 0) {
+        if (char === splitChar && curlyDepth == 0 && squareDepth == 0) {
             tokens.push(current);
             current = "";
             continue;
+        }
+
+        if (hasSplit) {
+            throw Error(`Unexpected text after ${name}: \n` + text.substring(i).trim().split("\n").map(t => "    " + t).join("\n") + "\nin:\n" + text2.split("\n").map(t => "    " + t).join("\n") + "\n");
         }
 
         current += char;
     }
 
     if (current) {
-        tokens.push(current);
+        tokens.push(current.trim());
     }
 
     return tokens;
+}
+
+function splitBlock(text) {
+    return split(text, "curly", "block");
+}
+
+function splitHeader(text) {
+    return split(text, "square", "header");
+}
+
+function splitSegment(text) {
+    return split(text, ",");
 }
 
 function splitKey(text) {
-    text = text.trim();
-    const tokens = [];
-    let current = "";
-
-    let depth = 0;
-    let inSingle = false;
-        inDouble = false;
-        inTick = false;
-    
-    for (let i = 0; i < text.length; i++) {
-        const char = text[i];
-
-        if (char == "\\") {
-            current += char;
-            continue;
-        }
-
-        if (char == "'" && !(inDouble || inTick))
-            inSingle = !inSingle;
-        if (char == "\"" && !(inSingle || inTick))
-            inDouble = !inDouble;
-        if (char == "`" && !(inSingle || inDouble))
-            inTick = !inTick;
-
-        const inQuotes = inSingle || inDouble || inTick;
-
-        if (inQuotes) {
-            current += char;
-            continue;
-        }
-
-        if (char === "=" && depth == 0) {
-            tokens.push(current);
-            current = "";
-            continue;
-        }
-
-        current += char;
-    }
-
-    if (current) {
-        tokens.push(current);
-    }
-
-    return tokens;
+    return split(text, "=");
 }
 
 const removeStr = (str) => str.replace(/\\(.)|["'`]/g, (match, escaped) => escaped || '');
-const isNumeric = t=>/^[+-]?(\d+(\.\d*)?|\.\d+)$/.test(t);
+const removeComments = (str) => str.replace(/(["'`])(?:(?=(\\?))\2.)*?\1|\/\/.*|\/\*[\s\S]*?\*\//g,((t,e)=>e?t:""))
 
 class AstSegment {
     constructor(code = null) {
@@ -249,7 +105,7 @@ class AstSegment {
         }
     }
     parse(code) {
-        const elements = splitSegment(code);
+        const elements = splitSegment(removeComments(code));
         this.elements = elements.map(n => new AstNode(n));
     }
     stringify() {
@@ -257,26 +113,27 @@ class AstSegment {
     }
 }
 
-class Ast extends AstSegment {}
+class Ast extends AstSegment {
+    stringify() {
+        return `Ast{${this.elements.map(n => n.stringify()).join(",")}}`
+    }
+}
 
 class AstNode {
     constructor(code = null, data = null) {
-        if (data) { // for stuff like new AstNode("block",{...});
-            this.kind = code;
-            this.data = data;
-            return;
-        }
-        
-        // parse the code and replace itself with it.
-        let n = AstNode.parse(code) ?? AstNode.unknown(code);
-        this.kind = n.kind;
-        this.data = n.data;
+        this.kind = "unknown";
+        this.data = code;
+
+        this.parse(code);
     }
     stringify() {
         let contents = "";
         switch (this.kind) {
             case "block":
-                contents = `header:${this.data.header.stringify()},contents:${this.data.content.stringify()}`
+                contents = `header:${this.data.header.stringify()},contents:${this.data.content.stringify()}`;
+                break;
+            case "element":
+                contents = `value:${this.data.value.stringify()},attributes:[${this.data.attributes.map(a => a.stringify()).join(",")}]`;
                 break;
             case "unknown":
                 contents = this.data.trim();
@@ -287,22 +144,35 @@ class AstNode {
     static unknown(code) {
         return new AstNode("unknown", code);
     }
-    static parse(code) {
-        if (code.trim() === "") return null;
+    parse(code) {
+        if (code.trim() === "") {
+            this.kind = "empty";
+            this.data = {};
+            return;
+        }
         
         const block = splitBlock(code);
+        const header = new AstHeader(block[0]);
         if (block.length == 2) {
-            const header = new AstBlockHeader(block[0]);
-            const content = header.type === "script" ? new AstScriptSegment(block[1]) : new AstSegment(block[1]);
-            return new AstNode("block",{
-                "header": header,
-                "content": content
-            })
+            const content = header.key === "script" ? new AstScriptSegment(block[1]) : new AstSegment(block[1]);
+            this.kind = "block";
+            this.data = {
+                header: header,
+                content: content
+            };
+            return;
         }
+        const value = new AstValue(header.key);
+        this.kind = "element";
+        this.data = {
+            value: value,
+            attributes: header.attributes
+        };
+        return;
     }
 }
 
-class AstBlockHeader {
+class AstHeader {
     constructor(code = "") {
         this.parse(code);
     }
@@ -313,25 +183,26 @@ class AstBlockHeader {
 
         if (header.length == 2) {
             const attributes = splitSegment(header[1]);
-            this.attributes = attributes.map(a => new AstBlockAttribute(a))
+            this.attributes = attributes.map(a => new AstAttribute(a))
         }
 
-        this.type = header[0];
+        this.key = header[0];
     }
     stringify() {
-        return `Header<${this.type}>${this.attributes.length > 0 ? "{" + this.attributes.map(a => a.stringify()).join(",") + "}" : ""}`;
+        return `Header<${this.key}>${this.attributes.length > 0 ? "{" + this.attributes.map(a => a.stringify()).join(",") + "}" : ""}`;
     }
 }
 
-class AstBlockAttribute {
+class AstAttribute {
     constructor(code = "") {
         this.parse(code);
     }
     parse(code) {
         const tokens = splitKey(code);
+
         if (tokens.length == 2) {
             this.kind = "key";
-            this.key = tokens[0];
+            this.key = tokens[0].trim();
             this.value = new AstValue(tokens[1]);
             return;
         }
@@ -352,6 +223,8 @@ class AstValue {
         this.parse(code);
     }
     parse(code) {
+        code = code.trim();
+        
         if (
             (code[0] === "\"" && code[code.length-1] === "\"") || 
             (code[0] === "'" && code[code.length-1] === "'") || 
@@ -361,17 +234,39 @@ class AstValue {
             this.value = removeStr(code);
             return;
         }
-
-        if (isNumeric(code)) {
+        
+        const num = Number(code);
+        if (!isNaN(num)) {
             this.type = "num";
-            this.value = Number(code);
+            this.value = num;
             return;
+        }
+
+        if (code[code.length-1] == "%") {
+            const num = Number(code.slice(0,-1));
+            if (!isNaN(num)) {
+                this.type = "percentage";
+                this.value = num;
+                return;
+            }
         }
 
         throw Error("Unknown value syntax: " + code);
     }
     stringify() {
-        return `Value<${this.type}>`;
+        let data = null;
+        switch (this.type) {
+            case "str":
+                data = this.value;
+                break;
+            case "num":
+                data = this.value.toString();
+                break;
+            case "percentage":
+                data = this.value.toString() + "%";
+                break;
+        }
+        return `Value<${this.type}>${data ?? "" !== "" ? `{${data}}` : ""}`;
     }
 }
 
@@ -386,13 +281,21 @@ class AstScriptSegment {
 
 const code = `
 root {
-  "rwl based website :D" [id = "myID"],
-  frame [flex] {
-  
-  },
-  script [type="rtr",interval=5] {
-    log("{")
-  }
+    "rwl based website :D" [id = "myID"],
+    frame [direction = "horizontal"] {
+        section [scale = 100] {
+            ":3"
+        },
+        section [scale = 50%] {
+            "wow"
+        },
+        section {
+            "crazy"
+        }
+    },
+    script [type="rtr",interval=5] {
+        log("hi")
+    }
 }
 `
 
